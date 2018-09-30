4aa40715eb7657f8ad8d8c62f908f73fa20698bc
gazeka74 <gazeka74@gmail.com>
2018-09-30 14:22:31 +0200

    Added some buttons, redrawing doesn't work -> might check the canvas clearing. Leaving works fine.

--- src/dynamic_queries/UserInterface.java
+++ src/dynamic_queries/UserInterface.java
@@ -1,14 +1,20 @@
 package dynamic_queries;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.paint.*;
@@ -17,26 +23,44 @@ import javafx.scene.canvas.*;
 public class UserInterface extends Application {
 
 	// =============================================
-	// ===== PARAMETERS OF THE APPLICATION
+	// ===== ATTRIBUTS
 	// =============================================
-
 	// Homes
 	private Home tab_homes[];
 
-	// =============================================
-	// ===== ATTRIBUTS
-	// =============================================
+	private int min_rooms;
+	private int max_rooms;
+	private int min_price;
+	private int max_price;
 
 	// Size of the canvas
 	// Used to calculate position
 	private int canvas_h = 1000;
 	private int canvas_w = 1000;
 
+	// Basic container
 	private SplitPane root;
+
+	// Left and right container
 	private BorderPane right_pane;
 	private StackPane left_pane;
+
+	// The canvas on the left
 	private Canvas canvas;
 	private GraphicsContext gc;
+
+	// An other container for the right
+	private VBox right_vbox;
+
+	// Containter of the title button
+	private BorderPane title_pane;
+	private Button title;
+
+	// Container of the two buttons reset and quit
+	private BorderPane reset_and_quit;
+	private Button reset;
+	private Button quit;
+	private DropShadow shadow;
 
 	// =============================================
 	// ===== METHODES
@@ -50,6 +74,12 @@ public class UserInterface extends Application {
 	public void start(Stage primaryStage) {
 		// Initiate the homes
 		initiateHouses();
+		
+		// Base value for out filters
+		min_rooms = Application_variables.min_rooms;
+		max_rooms = Application_variables.max_rooms;
+		min_price = Application_variables.min_price;
+		max_price = Application_variables.max_price;
 
 		// Contains all the components of the GUI
 		root = new SplitPane();
@@ -65,11 +95,85 @@ public class UserInterface extends Application {
 		// Where we draw the houses
 		canvas = new Canvas(canvas_w, canvas_h);
 		gc = canvas.getGraphicsContext2D();
-
 		drawHomes(gc);
 
 		// Add the canvas to the left_pane
 		left_pane.getChildren().add(canvas);
+
+		// The right discending button list
+		right_vbox = new VBox();
+		right_pane.setCenter(right_vbox);
+
+		// Initiate and add the title
+		title_pane = new BorderPane();
+		title = new Button("HOUSE PICKER");
+		title.setStyle("-fx-background-color: #c0c5ce; -fx-font-size:35");
+		title.maxWidth(Double.MAX_VALUE);
+		title.setDisable(true);
+		title_pane.setCenter(title);
+		BorderPane.setMargin(title, new Insets(20));
+
+		// Reset and quit
+		reset_and_quit = new BorderPane();
+		shadow = new DropShadow();
+
+		reset = new Button("RESET");
+		reset.setStyle("-fx-background-color: #c0c5ce; -fx-font-size:30");
+		reset.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Reset");
+				reset();
+			}
+		});
+
+		// Adding the shadow when the mouse cursor is on
+		reset.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				reset.setEffect(shadow);
+			}
+		});
+		// Removing the shadow when the mouse cursor is off
+		reset.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				reset.setEffect(null);
+			}
+		});
+		quit = new Button("EXIT");
+		quit.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Platform.exit()");
+				Platform.exit();
+			}
+		});
+		quit.setStyle("-fx-background-color: #c0c5ce; -fx-font-size:30");
+		// Adding the shadow when the mouse cursor is on
+		quit.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				quit.setEffect(shadow);
+			}
+		});
+		// Removing the shadow when the mouse cursor is off
+		quit.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				quit.setEffect(null);
+			}
+		});
+
+		reset_and_quit.setLeft(reset);
+		BorderPane.setMargin(reset, new Insets(20));
+		reset_and_quit.setRight(quit);
+		BorderPane.setMargin(quit, new Insets(20));
+
+		right_vbox.getChildren().add(title_pane);
+		right_vbox.getChildren().add(reset_and_quit);
 
 		// Add the 2 components to the base
 		root.getItems().add(left_pane);
@@ -79,6 +183,14 @@ public class UserInterface extends Application {
 		primaryStage.setTitle(" Another wonderful application by the Maitres Squaleurs :D");
 		primaryStage.setScene(new Scene(root, 1920, 1080));
 		primaryStage.show();
+	}
+
+	private void reset() {
+		min_rooms = Application_variables.min_rooms+2;
+		max_rooms = Application_variables.max_rooms-2;
+		min_price = Application_variables.min_price+10000;
+		max_price = Application_variables.max_price-10000;
+		drawHomes(gc);
 	}
 
 	// Handle the initalization of the nb_homes houses
@@ -118,6 +230,8 @@ public class UserInterface extends Application {
 			rooms = home.getNb_room();
 			value = home.getValue();
 
+			if (rooms < min_rooms || rooms > max_rooms || value < min_price || value > max_price)
+				continue;
 			p = translatePrice(value);
 			gc.setFill(p);
 			gc.setStroke(p);
